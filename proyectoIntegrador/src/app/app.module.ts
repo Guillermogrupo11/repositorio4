e6362a0c4b633679c1f9dd74aa9423c65f0642fc
Guillermo <guillermo.arena@gmail.com>
2022-04-26 21:57:04 -0300

    creacion de componentes
    funciona, agregue componente de bootstrap

--- proyectoIntegrador/src/app/app.module.ts
+++ proyectoIntegrador/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SocialComponent } from './components/social/social.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SocialComponent,
+    LogoApComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
