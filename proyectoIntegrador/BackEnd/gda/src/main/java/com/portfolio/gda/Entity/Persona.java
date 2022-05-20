969b2f838227dd70a58cd09fc3861e359a90a023
Guillermo <guillermo.arena@gmail.com>
2022-05-20 12:42:10 -0300

    modifique borrando crear, borrar, etc, de manera manual

--- proyectoIntegrador/BackEnd/gda/src/main/java/com/portfolio/gda/Entity/Persona.java
+++ proyectoIntegrador/BackEnd/gda/src/main/java/com/portfolio/gda/Entity/Persona.java
@@ -24,21 +24,4 @@ public class Persona {
     private String apellido;
     
     @Size( min = 1, max = 50, message = "no cumple con la longitud")
-    private String img;
-
-    public void setNombre(String nuevoNombre) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    public void setApellido(String nuevoApellido) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    public void setImg(String nuevoImg) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    
-    
-    
-}
+    private String img;}
