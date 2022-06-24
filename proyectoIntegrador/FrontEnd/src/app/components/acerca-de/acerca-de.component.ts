8b85f945c659a9ddd5ae314f585eb15b3ab013d1
Guillermo <guillermo.arena@gmail.com>
2022-06-24 16:36:12 -0300

    alguna mejoras en acerca de html y ts

--- proyectoIntegrador/FrontEnd/src/app/components/acerca-de/acerca-de.component.ts
+++ proyectoIntegrador/FrontEnd/src/app/components/acerca-de/acerca-de.component.ts
@@ -5,17 +5,20 @@ import { PersonaService } from 'src/app/service/persona.service';
 @Component({
   selector: 'app-acerca-de',
   templateUrl: './acerca-de.component.html',
-  styleUrls: ['./acerca-de.component.css']
+  styleUrls: ['./acerca-de.component.css'],
 })
 export class AcercaDeComponent implements OnInit {
-  persona: persona= new persona("","","");
+  persona!: any;
 
-  constructor(public personaService: PersonaService) {
+  constructor(public personaService: PersonaService) {}
 
-   }
-
-  ngOnInit(): void {
-    this.personaService.getPersona().subscribe(data =>{this.persona = data});
+  mostrarDatos() {
+      this.personaService.getPersona().subscribe((data) => {
+        this.persona = data;
+      })
   }
 
+  ngOnInit(): void {
+    this.mostrarDatos();
+  }
 }
