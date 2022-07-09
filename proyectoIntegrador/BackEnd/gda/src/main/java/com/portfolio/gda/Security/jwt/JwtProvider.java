3d6845b8877dbd2686741150b87dfface41bac26
Guillermo <guillermo.arena@gmail.com>
2022-07-09 19:50:14 -0300

    token termiando

--- proyectoIntegrador/BackEnd/gda/src/main/java/com/portfolio/gda/Security/jwt/JwtProvider.java
+++ proyectoIntegrador/BackEnd/gda/src/main/java/com/portfolio/gda/Security/jwt/JwtProvider.java
@@ -1,8 +1,12 @@
 package com.portfolio.gda.Security.jwt;
 
 import com.portfolio.gda.Security.Entity.UsuarioPrincipal;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +18,10 @@ import org.springframework.stereotype.Component;
 public class JwtProvider {
 
     private final static Logger logger = LoggerFactory.getLogger(JwtProvider.class);
+
+    static boolean validateToken(String token) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     @Value("$(jwt.secret)")
     private String secret;
     @Value("$(jwt.expiration)")
@@ -25,9 +33,26 @@ public class JwtProvider {
                 .setExpiration(new Date(new Date().getTime() + expiration * 1000))
                 .signWith(SignatureAlgorithm.HS512, secret).compact();
     }
-    public String getNombreUsuarioFromToken(String token){
-       return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().getSubject();
+
+    public String getNombreUsuarioFromToken(String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().getSubject();
     }
-    
-    public boolean 8 
+
+    public boolean vvalidateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+            return true;
+        } catch (MalformedJwtException e) {
+            logger.error("Token mal formado");
+        } catch (UnsupportedJwtException e) {
+            logger.error("Token no soportado");
+        } catch (ExpiredJwtException e) {
+            logger.error("Token expierado");
+        } catch (IllegalArgumentException e) {
+            logger.error("Token vacio");
+        } catch (SignatureException e) {
+            logger.error("Firma no valida");
+        }
+        return false;
+    }
 }
