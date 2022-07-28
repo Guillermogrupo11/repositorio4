99253e01a1752759c6695f759cd264ac1bee4a07
Guillermo <guillermo.arena@gmail.com>
2022-07-28 14:34:16 -0300

    ..

--- proyectoIntegrador/FrontEnd/src/app/service/persona.service.ts
+++ proyectoIntegrador/FrontEnd/src/app/service/persona.service.ts
@@ -7,7 +7,11 @@ import { persona } from '../model/persona.model';
   providedIn: 'root'
 })
 export class PersonaService {
-  URL = 'http://localhost:8080/personas/';
+
+  /**ESTAN YO AQUI EN TU FRONTEND Y TENIENDO LA URL LA CAMBIAS AQUI ABAJO*/
+  
+  //URL = 'http://localhost:8080/personas/';
+  URL = 'https://guillermoTuApellido.heroku.com'
 
   constructor(private http: HttpClient) { }
 
@@ -16,7 +20,55 @@ export class PersonaService {
     
   } */
 
+  //////////////////////////GET////////////////
+
+//Para traer los datos personales tuyos vas a hacer esto de aqui abajo.
   getPersona(){
     return this.http.get(`${this.URL}traer`)
   }
+//Para obtener la educacion vas hacer esto de abajo
+  getEducation(){
+    return this.http.get(`${this.URL}traer/educacion`)
+  }
+
+  //Para obtener la experiencian vas hacer esto de abajo
+  getExperiencia(){
+    return this.http.get(`${this.URL}traer/experiencia`)
+  }
+
+  //Para obtener la Proyectos vas hacer esto de abajo
+  getProyectos(){
+    return this.http.get(`${this.URL}traer/proyectos`)
+  }
+
+  //////////////////////////POST////////////////
+  //aqui vas hacer algo similar a lo de arriba, solo que vas a crear las funciones para actualizar/borrar los datos
+
+  //Para traer los datos personales tuyos vas a hacer esto de aqui abajo.
+  postPersona(){
+    return this.http.post(`${this.URL}actualizar`) // luego fijate los parametros que necesitan
+  }
+
+  postEducacion(){
+    return this.http.post(`${this.URL}actualizar`) // luego fijate los parametros que necesitan
+  }
+
+  postExperiencia(){
+    return this.http.post(`${this.URL}actualizar`) // luego fijate los parametros que necesitan
+  }
+
+  //////////////////////////DELETE////////////////
+  deleteExperiencia(){
+    return this.http.delete(`${this.URL}borrar`) // luego fijate los parametros que necesitan
+  }
+
+  deleteExperiencia(){
+    return this.http.delete(`${this.URL}borrar`) // luego fijate los parametros que necesitan
+  }
+
+  deleteExperiencia(){
+    return this.http.delete(`${this.URL}borrar`) // luego fijate los parametros que necesitan
+  }
+
+  //basicamente esto tendrías que hace para ir obteniendo los datos y mostrarlos en tu FRONTEND
 }
