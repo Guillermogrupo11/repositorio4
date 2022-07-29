217b629e2b688a5a634687fa750d31fef8d02197
Guillermo <guillermo.arena@gmail.com>
2022-07-28 21:21:29 -0300

    experiencia laboral en front

---
+++ proyectoIntegrador/FrontEnd/src/app/service/s-experiencia.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SExperienciaService {
+  expURL = 'https://localhost:8080/explab'
+
+  constructor() { }
+}
